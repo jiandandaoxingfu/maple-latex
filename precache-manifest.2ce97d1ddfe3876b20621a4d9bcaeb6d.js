f572e06b6288d3d64c387ca104f5e76242b3d8ab
jiandandaoxingfu <jiaminxin@outlook.com>
2019-11-02 16:58:51 +0800

    Updates

---
+++ precache-manifest.2ce97d1ddfe3876b20621a4d9bcaeb6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df4e008589f1d148ec86b6ac1fd2452",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3029495487643d971718",
+    "url": "./static/css/2.beb32c99.chunk.css"
+  },
+  {
+    "revision": "ce3956d4efde4097bb3b",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3029495487643d971718",
+    "url": "./static/js/2.6c1a54a3.chunk.js"
+  },
+  {
+    "revision": "ce3956d4efde4097bb3b",
+    "url": "./static/js/main.8a23438c.chunk.js"
+  },
+  {
+    "revision": "4655c71bd49d6b2e94bc",
+    "url": "./static/js/runtime-main.1e30857a.js"
+  }
+]);
