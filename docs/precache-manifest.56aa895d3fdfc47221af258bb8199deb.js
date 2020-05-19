fa7b1c02be3b8c98c13dd4d78f17eba323cacac4
jiandandaoxingfu <jiaminxin@outlook.com>
2020-05-19 13:10:33 +0800

    添加解析typora生成的md文档

---
+++ docs/precache-manifest.56aa895d3fdfc47221af258bb8199deb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d6fed79a6a556254f3d6189108721d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8794f746fdd26e8cbea5",
+    "url": "./static/css/2.32c9a41d.chunk.css"
+  },
+  {
+    "revision": "91eb464955b03d9bdfcc",
+    "url": "./static/css/main.d246c86f.chunk.css"
+  },
+  {
+    "revision": "8794f746fdd26e8cbea5",
+    "url": "./static/js/2.42b6844d.chunk.js"
+  },
+  {
+    "revision": "91eb464955b03d9bdfcc",
+    "url": "./static/js/main.2dc750a5.chunk.js"
+  },
+  {
+    "revision": "708b6f42c647a7bbd637",
+    "url": "./static/js/runtime-main.289c91a0.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6464f083d3fb77d532c20af955805366.js"
+  "./precache-manifest.56aa895d3fdfc47221af258bb8199deb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
