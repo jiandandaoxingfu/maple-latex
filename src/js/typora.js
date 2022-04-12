e41720f8045be9f85a7e12aa27778f44ed4def4b
JMx <jiaminxin@outlook.com>
2022-04-12 22:28:49 +0800

    update: typora
    - 增加对tex文件的支持. 目前可以解析章节标题, 数学公式.

--- src/js/typora.js
+++ src/js/typora.js
@@ -6,31 +6,62 @@ const md = new Remarkable({
   langPrefix: 'language-', // CSS language prefix for fenced blocks
 });
 
+function math_format(math) {
+  return math.replace(/(<|>)/g, ' $1 ')
+    .replace(/\\\\/g, '\\\\  ')
+    .replace(/\\(R|C|Z|N)([^a-zA-Z])/g, '\\mathbb{$1}$2')
+    .replace(/\\part([^i])/g, '\\partial$1')
+    .replace(/(^\$\$|\$\$$)/g, '$$$$$$')
+    .replaceAll('-AAAAAAA-', '\r\n');
+}
+
 export function typora() {
   // 解析typora文档，支持数学公式
   let file = document.getElementById('typora-upload').files[0];
   if (!file.name.match(/\.(md|tex)$/)) return
   let reader = new FileReader();
   reader.onload = function() {
-    let result = this.result;
+    let result = this.result.replace(/\r\n/g, '-AAAAAAA-')
+      .replace(/.*?\\begin{document}/, '')
+      .replaceAll('dfrac', 'frac')
+      .replace(/\\bm([^a-zA-Z])/g, ' $1')
+      .replace(/\\chapter{(.*?)}/g, '\r\n# $1\r\n')
+      .replace(/\\section{(.*?)}/g, '\r\n## $1\r\n')
+      .replace(/\\subsection{(.*?)}/g, '\r\n### $1\r\n')
+    // 将 tex 自定义命令替换
+    this.result.match(/\\newcommand{(.*?)}\[0\]{(.*?)}\r\n/g)
+      ?.map( cmd => {
+          let match = cmd.match(/\\newcommand{(.*?)}\[0\]{(.*?)}\r\n/);
+          if( match ) {
+            let reg = new RegExp(match[1].replaceAll('\\', '\\\\') + '([^a-zA-Z])', 'g');
+            result = result.replace(reg, match[2] + '$1');
+          }
+      });
     // 匹配所有数学公式，将其中</>号前后加空格，不然会被解析为html标签。
     // 将每一个公式替换为EQUATION-id，然后使用remarkable解析md，之后在替换回来，
     // 使用MathJax渲染。
-    let maths = result.match(/\${1,2}(\r\n|.){1,}?\${1,2}/g) || [];
-    maths = maths.map(math => {
-      result = result.replace(math, 'EQUATION-TO-REPLACE');
-      return math.replace(/(<|>)/g, ' $1 ')
-        .replace(/\\\\/g, '\\\\  ')
-        .replace(/\\(R|C|Z|N)([^a-zA-Z])/g, '\\mathbb{$1}$2')
-        .replace(/\\part([^i])/g, '\\partial$1')
-        .replace(/(^\$\$|\$\$$)/g, '$$$$$$'); // replace会把两个$$变成一个。
+    let math_inline = result.match(/\${1,2}.+?\${1,2}/g) || [];
+    let math_block = result.match(/\\begin{(equation|align|)\*?}.*?\\end{(equation|align|)\*?}/g) || [];
+    math_inline = math_inline.map(math => {
+      result = result.replace(math, 'EQUATION-TO-REPLACE1');
+      return math_format(math);
+    })
+    math_block = math_block.map(math => {
+      result = result.replace(math, 'EQUATION-TO-REPLACE2');
+      return math_format(math);
     })
 
-    result = result.replace(/(\r\n#+.*?\r\n)/g, '\r\n$1\r\n');
+    result = result.replace(/(\r\n#+.*?\r\n)/g, '\r\n$1\r\n')
+                   .replaceAll('-AAAAAAA-', '\r\n')
+                   .replace(/\\/g, '')
+                   .replace(/bibitem{(.*?)}/g, '- **$1**')
     result = md.render(result);
 
-    maths.forEach(math => {
-      result = result.replace('EQUATION-TO-REPLACE', math);
+    math_inline.forEach(math => {
+      result = result.replace('EQUATION-TO-REPLACE1', math);
+    })
+    math_block.forEach(math => {
+      result = result.replace('EQUATION-TO-REPLACE2', math);
     })
 
     document.getElementById('output').innerHTML = result;
