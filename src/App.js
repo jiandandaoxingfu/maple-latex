1a4807ededcc42214dc1153b74422602091dbd65
JMx <33108686+jiandandaoxingfu@users.noreply.github.com>
2023-03-03 07:52:36 +0800

    Update App.js
    无

--- src/App.js
+++ src/App.js
@@ -21,11 +21,8 @@ function App() {
         </textarea>
         <div style={ styles.footer }>  
           <span style={{ margin: '0px 10px' }}>
-            ©2018-2022 
+            ©2018-2023 
             <a style={{ color: 'black' }} rel="noreferrer" target="_blank" href="https://github.com/jiandandaoxingfu/maple-latex"><strong> Maple-Latex</strong></a> v2.6 &nbsp;
-            <a style={{ color: 'black' }} rel="noreferrer" target="_blank" href="https://jiandandaoxingfu.github.io/myblog/post/maple-latex/">
-              <strong>文档</strong>
-            </a>
           </span>
           <span></span>
           <span style={{ margin: '0 10px'}}>
@@ -33,10 +30,17 @@ function App() {
             <a style={{ color: 'black' }} rel="noreferrer" target="_blank" href="https://github.com/jiandandaoxingfu">
             <strong>  JMx  </strong>
             </a>
-            with ❤️ and 
+            with 
             <a style={{ color: 'black' }} rel="noreferrer" target="_blank" href="https://reactjs.org/">
-            <strong>  React  </strong>
+              <strong>  React  </strong>
             </a>
+            and
+            ❤️  
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a style={{ color: 'black' }} rel="noreferrer" target="_blank" href="https://jiandandaoxingfu.github.io/myblog/post/maple-latex/">
+              <strong>文档</strong>
+            </a>
+            
           </span>  
         </div>
       </div>
