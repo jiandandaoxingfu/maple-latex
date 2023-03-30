c2f2c7c42728e306ff32c9299ed5e4ce97567c74
JMx <33108686+jiandandaoxingfu@users.noreply.github.com>
2023-03-30 13:18:30 +0800

    Update maple2mma.js
    fix bug

--- src/js/maple2mma.js
+++ src/js/maple2mma.js
@@ -44,7 +44,7 @@ export const func_map = {
 
 export function get_maple_func() {
   const maple_expr = document.getElementById('input').value;
-  let maple_func = (' ' + maple_expr).match(/[^a-zA-Z\d_][a-zA-Z_][a-zA-Z\d_]*?\(/g)?.map( func => func.slice(1, -1) );
+  let maple_func = (' ' + maple_expr).match(/[a-zA-Z_][a-zA-Z\d_]*?\(/g)?.map( func => func.slice(0, -1) );
   maple_func = [ ...new Set(maple_func) ];
   maple_func.sort( (a, b) => (a.length < b.length) - 1 );
   return !maple_func?.[0] ? [] : maple_func;
@@ -56,9 +56,8 @@ export function maple2mma() {
   let lc = document.getElementById('input').value;
   maple_func_list.forEach( (func, i) => {
     if (func === 'diff') {
-      lc = lc.replace(/diff\(([^,]*?), ([a-zA-Z])\)/g, "D[$1, $2]"); 
       for(let i=1; i<10; i++) {
-        let re = new RegExp('diff\\(([a-zA-Z]+(\\(.*?\\))?)' + ', ([a-z])'.repeat(i) + '\\)', 'g');
+        let re = new RegExp('diff\\(([a-zA-Z_][a-zA-Z_\\d]*?([\\(\\[][^\\[\\]\\(\\)]*?[\\)\\]])?)' + ', ([a-z])'.repeat(i) + '\\)', 'g');
         lc = lc.replace(re, `D[$1, {$3, ${i}}]`);
       }
       lc = lc.replace(/diff\(([^,]*?), ([a-zA-Z])\$(\d+)\)/g, "D[$1, {$2, $3}]");
