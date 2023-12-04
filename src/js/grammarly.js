71114a9cc8dac8ea19520d4b3f0fe6e74a358417
JMx <33108686+jiandandaoxingfu@users.noreply.github.com>
2023-12-04 11:04:22 +0800

    Update grammarly.js

--- src/js/grammarly.js
+++ src/js/grammarly.js
@@ -7,8 +7,8 @@ export function grammarly() {
   lc = lc.replace(/\\ref{.*?}/g, '(3)');
   lc = lc.replace(/\\label{.*?}/g, '');
   lc = lc.replace(/\$.*?\$/g, '$A=B$');
-  lc = lc.replace(/\\begin{align\*?}(.|\n)*?\\end{align\*?}/g, '$$A=B$$');
-  lc = lc.replace(/\\begin{equation\*?}(.|\n)*?\\end{equation\*?}/g, '$$A=B$$');
+  lc = lc.replace(/\\begin{align\*?}(.|\n)*?\\end{align\*?}/g, '$$$$A=B$$$$,');
+  lc = lc.replace(/\\begin{equation\*?}(.|\n)*?\\end{equation\*?}/g, '$$$$A=B$$$$,');
   lc = lc.replace(/\\begin{thebibliography\*?}(.|\n)*?\\end{thebibliography\*?}/g, '');
   lc = lc.replace(/\d{8}/g, '1');
   lc = lc.replace(/(.|\n)*\\begin{abstract}/,'');
